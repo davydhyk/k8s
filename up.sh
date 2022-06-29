114e56928fedbca243594c3aa7c4332fa746f7cc
Davyd Huk <davydhyk@gmail.com>
2022-06-29 04:29:11 +0300

    setting up k8s and up script

--- up.sh
+++ up.sh
@@ -2,9 +2,11 @@ NAMESPACE=$(git rev-parse --abbrev-ref HEAD);
 
 kubectl create namespace $NAMESPACE;
 
+docker build -t migrations:$NAMESPACE api/prisma
 docker build -t client:$NAMESPACE client
 docker build -t api:$NAMESPACE api
 
+kubectl apply -f k8s/db.yaml --namespace $NAMESPACE;
 kubectl apply -f k8s/api.yaml --namespace $NAMESPACE;
 kubectl apply -f k8s/client.yaml --namespace $NAMESPACE;
 kubectl apply -f k8s/ingress.yaml --namespace $NAMESPACE;
