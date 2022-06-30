ffe266e8878031c5ed12ffda7a76f2140aab4084
Davyd Huk <davydhyk@gmail.com>
2022-06-30 07:14:35 +0300

    updating setup script to cover new deploymetns

--- up.sh
+++ up.sh
@@ -5,7 +5,10 @@ kubectl create namespace $NAMESPACE;
 docker build -t migrations:$NAMESPACE api/prisma
 docker build -t client:$NAMESPACE client
 docker build -t api:$NAMESPACE api
+docker build -t consumer:$NAMESPACE consumer
 
+kubectl apply -f k8s/rabbitmq.yaml --namespace $NAMESPACE;
+kubectl apply -f k8s/consumer.yaml --namespace $NAMESPACE;
 kubectl apply -f k8s/db.yaml --namespace $NAMESPACE;
 kubectl apply -f k8s/api.yaml --namespace $NAMESPACE;
 kubectl apply -f k8s/client.yaml --namespace $NAMESPACE;
